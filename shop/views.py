9687e77dfc8b05f92688516e55338af72569bd9c
sahilpysquad <sahilkhirsaria.pysquad@gmail.com>
2021-12-03 12:10:07 +0530

    final all. city and area.

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, CreateView
 
 from shop.forms import CreateCityForm
-from shop.models import City
+from shop.models import City, AreaZone
 
 
 class AllCities(ListView):
@@ -13,4 +13,16 @@ class AllCities(ListView):
 class CreateCity(CreateView):
     form_class = CreateCityForm
     template_name = "shopemanagement/createcity.html"
-    success_url = "/shoppermanagement/allcities/"
+    success_url = "/shop/allcities/"
+
+
+class CreateAreaZone(CreateView):
+    model = AreaZone
+    fields = ['name','city', 'pincode']
+    success_url = "/shop/allareazones/"
+    template_name = "shopemanagement/createareazone.html"
+
+
+class AreaZoneListView(ListView):
+    model = AreaZone
+    template_name = "shopemanagement/allareazone.html"
